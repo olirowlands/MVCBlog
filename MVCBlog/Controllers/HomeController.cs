c8e6af6d4966358be626c89859282ad231b3b522
olirowlands <oli.rowlands@gmail.com>
2018-12-09 16:18:26 +0000

    * HomeController.cs: Umm just figuring stuff out
    * About.cshtml:
    * Index.cshtml:
    * Contact.cshtml:

--- MVCBlog/Controllers/HomeController.cs
+++ MVCBlog/Controllers/HomeController.cs
@@ -29,10 +29,10 @@ namespace MVCBlog.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+       //public IActionResult Privacy()
+       //{
+       //    return View();
+       //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
